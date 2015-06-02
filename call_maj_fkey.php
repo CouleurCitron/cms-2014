6eca730017227fdc34e4d69da71d30ddd8e70b46
Raphael <raphael@couleur-citron.com>
2015-06-02 15:52:43 +0200

    mise à jour système de cache

--- call_maj_fkey.php
+++ call_maj_fkey.php
@@ -1,8 +1,10 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/lib/phpfastcache/php_fast_cache.php'); 
-phpFastCache::$storage = "auto";
+phpFastCache::setup("storage","auto");
+//phpFastCache::$storage = "auto";
+$cache = phpFastCache();
 	
-$html = phpFastCache::get(md5($_SERVER['REQUEST_URI']));
+$html = $cache->get(md5($_SERVER['REQUEST_URI']));
 if($html == null) {	
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_cms.php');
@@ -11,7 +13,7 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_class.php');
 	ob_start();
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/autoClass/maj.fkey.php');
 	$html = ob_get_flush();
-	phpFastCache::set(md5($_SERVER['REQUEST_URI']),$html,60);
+	$cache->set(md5($_SERVER['REQUEST_URI']),$html,60);
 }
 else{
 	echo $html;
