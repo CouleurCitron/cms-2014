865d25946c73afdce95c25bef731dbcec22c7d9a
Pierre Priot <pierre@couleur-citron.com>
2017-11-14 16:54:30 +0100

    fix path includes

--- call_maj_fkey.php
+++ call_maj_fkey.php
@@ -1,17 +1,18 @@
 <?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
+
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/lib/phpfastcache/php_fast_cache.php'); 
 phpFastCache::setup("storage","auto");
 //phpFastCache::$storage = "auto";
 $cache = phpFastCache();
 	
 $html = $cache->get(md5($_SERVER['REQUEST_URI']));
-if($html == null) {	
-include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_cms.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_class.php');
+if($html == null) {
+	include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_cms.php');
+	include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_class.php');
 
 	ob_start();
-include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/autoClass/maj.fkey.php');
+	include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/autoClass/maj.fkey.php');
 	$html = ob_get_flush();
 	$cache->set(md5($_SERVER['REQUEST_URI']),$html,60);
 }
