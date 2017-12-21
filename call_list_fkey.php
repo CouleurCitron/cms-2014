a84079cd0e87f35a681484032a530b69d4a9107e
Pierre Priot <pierre@couleur-citron.com>
2017-12-21 09:06:07 +0100

    chemins d'inclusion

--- call_list_fkey.php
+++ call_list_fkey.php
@@ -1,4 +1,6 @@
 <?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
+
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/lib/phpfastcache/php_fast_cache.php'); 
 phpFastCache::setup("storage","auto");
 //phpFastCache::$storage = "auto";
@@ -6,7 +8,7 @@ $cache = phpFastCache();
 	
 $html = $cache->get(md5($_SERVER['REQUEST_URI']));
 if($html == null) {	
-include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
+
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_cms.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/include_class.php');
 
