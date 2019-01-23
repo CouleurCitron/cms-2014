fc1824260fd01fa7c66c510613607ba1d1b59256
Pierre Priot <pierre@couleur-citron.com>
2019-01-23 09:03:19 +0100

    tarte au citron js lib

---
+++ js/tarteaucitron/lang/tarteaucitron.fr.js
@@ -0,0 +1,65 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+    "adblock": "Bonjour! Ce site joue la transparence et vous donne le choix des services tiers &agrave; activer.",
+    "adblock_call": "Merci de d&eacute;sactiver votre adblocker pour commencer la personnalisation.",
+    "reload": "Recharger la page",
+    
+    "alertBigScroll": "En continuant de d&eacute;filer,",
+    "alertBigClick": "En poursuivant votre navigation,",
+    "alertBig": "vous acceptez l'utilisation de services tiers pouvant installer des cookies",
+    
+    "alertBigPrivacy": "Ce site utilise des cookies et vous donne le contr&ocirc;le sur ce que vous souhaitez activer",
+    "alertSmall": "Gestion des services",
+    "acceptAll": "OK, tout accepter",
+    "personalize": "Personnaliser",
+    "close": "Fermer",
+
+    "all": "Pr&eacute;f&eacute;rence pour tous les services",
+
+    "info": "Protection de votre vie priv&eacute;e",
+    "disclaimer": "En autorisant ces services tiers, vous acceptez le d&eacute;p&ocirc;t et la lecture de cookies et l'utilisation de technologies de suivi n&eacute;cessaires &agrave; leur bon fonctionnement.",
+    "allow": "Autoriser",
+    "deny": "Interdire",
+    "noCookie": "Ce service ne d&eacute;pose aucun cookie.",
+    "useCookie": "Ce service peut d&eacute;poser",
+    "useCookieCurrent": "Ce service a d&eacute;pos&eacute;",
+    "useNoCookie": "Ce service n'a d&eacute;pos&eacute; aucun cookie.",
+    "more": "En savoir plus",
+    "source": "Voir le site officiel",
+    "credit": "Gestion des cookies par tarteaucitron.js",
+    
+    "fallback": "est d&eacute;sactiv&eacute;.",
+
+    "ads": {
+        "title": "R&eacute;gies publicitaires",
+        "details": "Les r&eacute;gies publicitaires permettent de g&eacute;n&eacute;rer des revenus en commercialisant les espaces publicitaires du site."
+    },
+    "analytic": {
+        "title": "Mesure d'audience",
+        "details": "Les services de mesure d'audience permettent de g&eacute;n&eacute;rer des statistiques de fr&eacute;quentation utiles &agrave; l'am&eacute;lioration du site."
+    },
+    "social": {
+        "title": "R&eacute;seaux sociaux",
+        "details": "Les r&eacute;seaux sociaux permettent d'am&eacute;liorer la convivialit&eacute; du site et aident &agrave; sa promotion via les partages."
+    },
+    "video": {
+        "title": "Vid&eacute;os",
+        "details": "Les services de partage de vid&eacute;o permettent d'enrichir le site de contenu multim&eacute;dia et augmentent sa visibilit&eacute;."
+    },
+    "comment": {
+        "title": "Commentaires",
+        "details": "Les gestionnaires de commentaires facilitent le d&eacute;p&ocirc;t de vos commentaires et luttent contre le spam."
+    },
+    "support": {
+        "title": "Support",
+        "details": "Les services de support vous permettent d'entrer en contact avec l'&eacute;quipe du site et d'aider &agrave; son am&eacute;lioration."
+    },
+    "api": {
+        "title": "APIs",
+        "details": "Les APIs permettent de charger des scripts : g&eacute;olocalisation, moteurs de recherche, traductions, ..."
+    },
+    "other": {
+        "title": "Autre",
+        "details": "Services visant &agrave; afficher du contenu web."
+    }
+};
